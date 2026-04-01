64fc71a0fed2b29ca214c6de258de8e00c98cc98
luger-mano <lucas.germanysantos@gmail.com>
2026-04-01 20:34:55 -0300

    [remove] : Removi alguns atributos e funcionalidades que não estavam sendo utilizados

--- src/main/java/org/ondedoar/domain/model/Address.java
+++ src/main/java/org/ondedoar/domain/model/Address.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.ondedoar.domain.enums.Zone;
 
-import java.util.UUID;
-
 @Entity
 @Table(name = "TB_ADDRESSES")
 @Getter
@@ -33,9 +31,6 @@ public class Address {
     @Column(name = "estado")
     private String estado;
 
-    @Column(name = "regiao")
-    private String regiao;
-
     @Column(name = "area")
     private String area;
 
@@ -52,27 +47,20 @@ public class Address {
     @Column(name = "municipio")
     private String municipio;
 
-    private UUID bloodCenterId;
-
-    private UUID userId;
-
     public Address() {
     }
 
-    public Address(Long addressId, String fullAddress, String cep, String bairro, String uf, String estado, String regiao, String area, String latitude, String longitude, Zone zone, String municipio, UUID bloodCenterId, UUID userId) {
+    public Address(Long addressId, String fullAddress, String cep, String bairro, String uf, String estado, String area, String latitude, String longitude, Zone zone, String municipio) {
         this.addressId = addressId;
         this.fullAddress = fullAddress;
         this.cep = cep;
         this.bairro = bairro;
         this.uf = uf;
         this.estado = estado;
-        this.regiao = regiao;
         this.area = area;
         this.latitude = latitude;
         this.longitude = longitude;
         this.zone = zone;
         this.municipio = municipio;
-        this.bloodCenterId = bloodCenterId;
-        this.userId = userId;
     }
 }
