a6415b034cc6b5cb414207171ba7fec169ca8c1a
luger-mano <lucas.germanysantos@gmail.com>
2026-04-07 20:59:03 -0300

    [feat] : Removi anotações de validation desnecessárias em AddressRequestDto e adicionei novo atributo para AddressResponseDto

--- src/main/java/org/ondedoar/adapter/request/address/AddressRequestDto.java
+++ src/main/java/org/ondedoar/adapter/request/address/AddressRequestDto.java
@@ -1,6 +1,5 @@
 package org.ondedoar.adapter.request.address;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.ondedoar.domain.enums.Zone;
@@ -9,19 +8,13 @@ import org.ondedoar.domain.enums.Zone;
 @Setter
 public class AddressRequestDto {
 
-    @NotBlank(message = "Campo 'fullAddress' deve ser preenchido")
     private String fullAddress;
     private String cep;
     private String bairro;
-    @NotBlank(message = "Campo 'estado' deve ser preenchido")
     private String estado;
-    @NotBlank(message = "Campo 'area' deve ser preenchido")
     private String area;
-    @NotBlank(message = "Campo 'latitude' deve ser preenchido")
     private String latitude;
-    @NotBlank(message = "Campo 'longitude' deve ser preenchido")
     private String longitude;
-    @NotBlank(message = "Campo 'zone' deve ser preenchido")
     private Zone zone;
     private String municipio;
 }
