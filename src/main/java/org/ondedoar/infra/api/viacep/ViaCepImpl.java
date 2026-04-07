6cbca12d8fcadac901dacb994f22419c3b66e7e2
luger-mano <lucas.germanysantos@gmail.com>
2026-04-07 20:58:02 -0300

    [feat] : Configurei e adicionei lógica de negócio para integrar com a API ViaCep, buscando o endereço pelo cep e retornando a entidade Address, usando Gson para lidar com Json da resposta e mappear para entidade.

---
+++ src/main/java/org/ondedoar/infra/api/viacep/ViaCepImpl.java
@@ -0,0 +1,50 @@
+package org.ondedoar.infra.api.viacep;
+
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.ondedoar.domain.model.Address;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Slf4j
+@Service
+public class ViaCepImpl implements ViaCepService {
+
+    @Override
+    public Address searchByCep(String cep) {
+        try {
+            Gson gson = new Gson();
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://viacep.com.br/ws/" +
+                            cep + "/json/"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String responseBody = response.body();
+
+            if (!responseBody.isBlank()) {
+                log.info("[AddressServiceImpl] - Object Java serializado: {}", responseBody);
+                return gson.fromJson(responseBody, Address.class);
+            }
+
+            log.info("Não foi possível serializar o object Java para Json");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        } catch (RuntimeException | InterruptedException | IOException e) {
+            log.error("Erro ao serializar o object Java para Json: {}", e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
