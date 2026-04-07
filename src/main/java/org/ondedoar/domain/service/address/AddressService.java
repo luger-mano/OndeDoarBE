2454d27c79805a289bc41de1f54a23acd1576453
luger-mano <lucas.germanysantos@gmail.com>
2026-04-07 20:55:50 -0300

    [feat] : Adicionei log e verificação de argumento, alterei o tipo de argumento de AddressRequestDto para Address

--- src/main/java/org/ondedoar/domain/service/address/AddressService.java
+++ src/main/java/org/ondedoar/domain/service/address/AddressService.java
@@ -1,12 +1,8 @@
 package org.ondedoar.domain.service.address;
 
-import org.ondedoar.adapter.request.address.AddressRequestDto;
-import org.ondedoar.adapter.response.address.AddressResponseDto;
 import org.ondedoar.domain.model.Address;
-
-import java.util.UUID;
 
 public interface AddressService {
 
-    Address createAddress(AddressRequestDto requestDto);
+    void createAddress(Address request);
 }
--- src/main/java/org/ondedoar/domain/service/address/AddressServiceImpl.java
+++ src/main/java/org/ondedoar/domain/service/address/AddressServiceImpl.java
@@ -1,13 +1,16 @@
 package org.ondedoar.domain.service.address;
 
 import jakarta.transaction.Transactional;
-import org.ondedoar.adapter.request.address.AddressRequestDto;
+import lombok.extern.slf4j.Slf4j;
 import org.ondedoar.domain.model.Address;
 import org.ondedoar.domain.repository.AddressRepository;
 import org.ondedoar.utils.mapper.AddressMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 
+@Slf4j
 @Service
 public class AddressServiceImpl implements AddressService {
 
@@ -19,18 +22,21 @@ public class AddressServiceImpl implements AddressService {
         this.addressMapper = addressMapper;
     }
 
-
     @Override
     @Transactional
-    public Address createAddress(AddressRequestDto requestDto){
+    public void createAddress(Address request) {
         try {
-            Address address = addressMapper.addressRequestDtoToAddress(requestDto);
 
-            addressRepository.save(address);
+            if (request == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "No Address found - [Path] : UserServiceImpl -> AddressServiceImpl");
+            }
 
-            return address;
-
+            log.info("Address saved");
+            Address addressSaved = addressRepository.save(request);
+            addressMapper.addressToAddressResponseDto(addressSaved);
         } catch (Exception e) {
+            log.error("Error saving address to database");
             throw new RuntimeException(e);
         }
     }
