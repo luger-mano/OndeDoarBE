02d28a8b59f04a5ab13e50d62949038994282e6b
luger-mano <lucas.germanysantos@gmail.com>
2026-04-08 02:04:46 -0300

    [feat] : Adicionei novo método returnCoordinatesByLogradouroAndLocalidadeAndCep() para pegar as coordenadas do endereço do User e salvar no banco de dados

--- src/main/java/org/ondedoar/infra/api/openstreetmap/OpenStreetMapImpl.java
+++ src/main/java/org/ondedoar/infra/api/openstreetmap/OpenStreetMapImpl.java
@@ -1,26 +1,32 @@
 package org.ondedoar.infra.api.openstreetmap;
 
 import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ondedoar.adapter.request.openstreet.GeolocationDestinationRequestDto;
 import org.ondedoar.adapter.request.openstreet.GeolocationStartingPointRequestDto;
+import org.ondedoar.adapter.request.user.UserCoordinatesRequestDto;
 import org.ondedoar.adapter.response.openstreet.OpenStreetMapRouteResponseDto;
 import org.ondedoar.adapter.response.openstreet.RouteResponseDto;
+import org.ondedoar.adapter.response.user.UserCoordinatesResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Component
-public class OpenStreetMapImpl implements OpenStreetMapService {
+@RequiredArgsConstructor
+public class OpenStreetMapImpl implements OpenStreetMapService, UserGeolocationService{
 
     @Override
     public OpenStreetMapRouteResponseDto routeByGeolocation(GeolocationStartingPointRequestDto startingPointRequestDto, GeolocationDestinationRequestDto destinationRequestDto) {
@@ -72,6 +78,55 @@ public class OpenStreetMapImpl implements OpenStreetMapService {
 
         } catch (RuntimeException | InterruptedException | IOException e) {
             log.error("Erro ao serializar o object Java para Json | drivingRouteByGeolocation: {}", e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Override
+    public UserCoordinatesResponseDto returnCoordinatesByLogradouroAndLocalidadeAndCep(UserCoordinatesRequestDto requestDto) {
+        try {
+            Gson gson = new Gson();
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            String query = String.format(
+                    "%s %s %s",
+                    requestDto.getLogradouro(),
+                    requestDto.getLocalidade(),
+                    requestDto.getCep()
+                            .replaceAll("\\D", "")
+                            .replaceAll("(\\d{5})(\\d3)","$1-$2")
+            );
+
+            String url = "https://nominatim.openstreetmap.org/search?q=" +
+                    URLEncoder.encode(query, StandardCharsets.UTF_8) +
+                    "&format=json&addressdetails=1&limit=1&polygon_svg=1";
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String responseBody = response.body();
+
+            if (!responseBody.isBlank()) {
+                log.info("[OpenStreetMapImpl | returnGeolocationByLogradouroAndLocalidadeAndBairro] -" +
+                        " Object Java serializado: {}", responseBody);
+
+                UserCoordinatesResponseDto[] responseArray =
+                        gson.fromJson(responseBody, UserCoordinatesResponseDto[].class);
+
+                if (responseArray.length > 0) {
+                    return responseArray[0];
+                }
+            }
+
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        } catch (RuntimeException | InterruptedException | IOException e) {
+            log.error("Erro ao serializar o object Java para Json | returnGeolocationByLogradouroAndLocalidadeAndBairro: {}", e.getMessage());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
     }
