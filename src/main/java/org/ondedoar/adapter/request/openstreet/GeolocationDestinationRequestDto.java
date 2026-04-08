ab9bfcc44ce3290fac20f25747521887ae2e2fca
luger-mano <lucas.germanysantos@gmail.com>
2026-04-07 22:59:34 -0300

    [feat] : Criei Dtos de request e response para geolocalização de partida e destino

---
+++ src/main/java/org/ondedoar/adapter/request/openstreet/GeolocationDestinationRequestDto.java
@@ -0,0 +1,12 @@
+package org.ondedoar.adapter.request.openstreet;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GeolocationDestinationRequestDto {
+
+    private String latitudeDestination;
+    private String longitudeDestination;
+}
