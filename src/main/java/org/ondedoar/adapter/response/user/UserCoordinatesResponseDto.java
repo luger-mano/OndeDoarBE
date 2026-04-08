94c87523665da55519a7fca0507a0aa130f69848
luger-mano <lucas.germanysantos@gmail.com>
2026-04-08 02:05:46 -0300

    [feat] : Criei DTOs request e response de UserCoordinatesRequestDto e UserCoordinatesResponseDto somente com os dados necessários para receber as coordenadas

---
+++ src/main/java/org/ondedoar/adapter/response/user/UserCoordinatesResponseDto.java
@@ -0,0 +1,11 @@
+package org.ondedoar.adapter.response.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCoordinatesResponseDto {
+    private String lat;
+    private String lon;
+}
