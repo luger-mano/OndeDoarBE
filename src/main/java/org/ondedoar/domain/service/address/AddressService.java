c0300294eeb9362586b88e7eab6a0edbc8e829c4
luger-mano <lucas.germanysantos@gmail.com>
2026-04-29 15:27:05 -0300

    [feat] : Adicionei novo método com regra de negócio buscando os endereços através do filtro passado por parametro e retornando DTO com stream map.

--- src/main/java/org/ondedoar/domain/service/address/AddressService.java
+++ src/main/java/org/ondedoar/domain/service/address/AddressService.java
@@ -1,9 +1,14 @@
 package org.ondedoar.domain.service.address;
 
 import org.ondedoar.adapter.request.address.AddressRequestDto;
+import org.ondedoar.adapter.response.address.AddressFilterRegionsResponseDto;
 import org.ondedoar.domain.model.Address;
+
+import java.util.List;
 
 public interface AddressService {
 
     Address createAddress(AddressRequestDto request);
+
+    List<AddressFilterRegionsResponseDto> filterByRegion(String filter, String value);
 }
