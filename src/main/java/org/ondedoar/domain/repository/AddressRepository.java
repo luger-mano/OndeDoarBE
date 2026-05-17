319f3e197760e4638b2228833df0368dbecea36e
luger-mano <lucas.germanysantos@gmail.com>
2026-05-17 16:14:01 -0300

    [feat] : Adicionei id de user para calcular a rota do endereço de User com o de BloodCenter

--- src/main/java/org/ondedoar/domain/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ondedoar.domain.repository;
-
-import org.ondedoar.domain.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface AddressRepository extends JpaRepository<Address, Long>, JpaSpecificationExecutor<Address> {
-
-}
