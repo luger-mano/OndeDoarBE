af081573bf85715e40215aff12cb733aac6a6de1
luger-mano <lucas.germanysantos@gmail.com>
2026-05-19 18:24:41 -0300

    [fix] : Arrumei o retorno do método de pesquisa getBloodCentersByAddressContainingIgnoreCase() para BloodCenterResponseDto

--- src/main/java/org/ondedoar/domain/service/bloodcenter/BloodCenterServiceImpl.java
+++ src/main/java/org/ondedoar/domain/service/bloodcenter/BloodCenterServiceImpl.java
@@ -2,7 +2,6 @@ package org.ondedoar.domain.service.bloodcenter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ondedoar.adapter.response.bloodcenter.BloodCenterBySearchResponseDto;
 import org.ondedoar.adapter.response.bloodcenter.BloodCenterResponseDto;
 import org.ondedoar.domain.model.bloodcenter.BloodCenter;
 import org.ondedoar.domain.model.bloodcenter.BloodCenterSpecifications;
@@ -22,12 +21,12 @@ public class BloodCenterServiceImpl implements BloodCenterService {
 
     private final BloodCenterMapper bloodCenterMapper;
     private final BloodCenterRepository bloodCenterRepository;
+    private final String BUCKET_S3_URL_IMAGE = "https://blood-centers-images.s3.us-east-1.amazonaws.com/";
 
     @Override
     @Cacheable("bloodCenters")
     public List<BloodCenterResponseDto> getAllBloodCenters() {
 
-        final String BUCKET_S3_URL_IMAGE = "https://blood-centers-images.s3.us-east-1.amazonaws.com/";
 
         return bloodCenterRepository
                 .findAll()
@@ -37,7 +36,7 @@ public class BloodCenterServiceImpl implements BloodCenterService {
                             bloodCenterMapper.bloodCenterToBloodCenterResponseDto(bloodCenter);
                     dto.setFacadeImageUrl(BUCKET_S3_URL_IMAGE + "bloodcenter.svg");
 
-                    verifyBloodCenterRegionImages(bloodCenter, dto, BUCKET_S3_URL_IMAGE);
+                    verifyBloodCenterRegionImages(bloodCenter, dto);
 
                     String status = BloodCenterOpeningValidator.validate(bloodCenter.getOperation());
                     dto.setOperation(status);
@@ -47,22 +46,32 @@ public class BloodCenterServiceImpl implements BloodCenterService {
                 .collect(Collectors.toList());
     }
 
-    private static void verifyBloodCenterRegionImages(BloodCenter bloodCenter, BloodCenterResponseDto dto, String BUCKET_S3_URL_IMAGE) {
+    private static void verifyBloodCenterRegionImages(BloodCenter bloodCenter, BloodCenterResponseDto dto) {
         if (bloodCenter.getBloodCenterAddress().getMunicipio() == null) {
-            dto.setNeighborhoodImageUrl(BUCKET_S3_URL_IMAGE + "neighborhood.svg");
+            dto.setNeighborhoodImageUrl("https://blood-centers-images.s3.us-east-1.amazonaws.com/" + "neighborhood.svg");
         } else {
-            dto.setMunicipalityImageUrl(BUCKET_S3_URL_IMAGE + "municipality.svg");
+            dto.setMunicipalityImageUrl("https://blood-centers-images.s3.us-east-1.amazonaws.com/" + "municipality.svg");
         }
     }
 
     @Override
-    public List<BloodCenterBySearchResponseDto> getBloodCentersByAddressContainingIgnoreCase(String search) {
+    public List<BloodCenterResponseDto> getBloodCentersByAddressContainingIgnoreCase(String search) {
+
         Specification<BloodCenter> bloodCenterSpecification = BloodCenterSpecifications.searchByTerm(search);
         List<BloodCenter> centers = bloodCenterRepository.findAll(bloodCenterSpecification);
 
         return centers.stream()
-                .map(bloodCenterMapper::bloodCenterEntityToBloodCenterBySearchResponseDto)
-                .collect(Collectors.toList());
+                .map(bloodCenter -> {
+                    BloodCenterResponseDto dto =
+                            bloodCenterMapper.bloodCenterToBloodCenterResponseDto(bloodCenter);
+                    dto.setFacadeImageUrl(BUCKET_S3_URL_IMAGE + "bloodcenter.svg");
+
+                    verifyBloodCenterRegionImages(bloodCenter, dto);
+
+                    String status = BloodCenterOpeningValidator.validate(bloodCenter.getOperation());
+                    dto.setOperation(status);
+
+                    return dto;}).collect(Collectors.toList());
     }
 
 }
