6a45f23513c3824e6167dd5696b5824a0e0fc19a
luger-mano <lucas.germanysantos@gmail.com>
2026-05-20 20:25:02 -0300

    [fix] : Arrumando problema com CORS

--- src/main/java/org/ondedoar/adapter/controller/UserController.java
+++ src/main/java/org/ondedoar/adapter/controller/UserController.java
@@ -25,7 +25,6 @@ public class UserController {
 
     private final UserService userService;
 
-
     @Operation(
             summary = "Create user",
             description = "Creates a new user resource in the system."
