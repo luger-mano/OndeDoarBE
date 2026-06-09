33cc6e7d4a7401d3f90f48eb150a33f97a5d719c
luger-mano <lucas.germanysantos@gmail.com>
2026-06-09 13:59:07 -0300

    [feat] : Criei novo método para gerar token de verficação de conta

--- src/main/java/org/ondedoar/domain/service/security/TokenService.java
+++ src/main/java/org/ondedoar/domain/service/security/TokenService.java
@@ -3,9 +3,17 @@ package org.ondedoar.domain.service.security;
 
 import org.ondedoar.adapter.request.security.LoginRequestDto;
 import org.ondedoar.adapter.response.security.LoginResponseDto;
+import org.ondedoar.domain.model.User;
 
 import javax.security.auth.login.CredentialException;
 
 public interface TokenService {
     LoginResponseDto generatesToken(LoginRequestDto requestDto) throws CredentialException;
+
+    String generateEmailVerificationToken(User user);
+
+    LoginResponseDto generateTokenForUser(User user);
+
+    String generatePasswordResetToken(User user);
+
 }
--- src/main/java/org/ondedoar/domain/service/security/TokenServiceImpl.java
+++ src/main/java/org/ondedoar/domain/service/security/TokenServiceImpl.java
@@ -7,17 +7,19 @@ import org.ondedoar.adapter.response.security.LoginResponseDto;
 import org.ondedoar.domain.model.User;
 import org.ondedoar.domain.repository.UserRepository;
 import org.ondedoar.infra.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.security.auth.login.CredentialException;
 import java.time.Instant;
 
-@Service
 @Slf4j
+@Service
 public class TokenServiceImpl implements TokenService {
 
     private final JwtEncoder jwtEncoder;
@@ -33,36 +35,71 @@ public class TokenServiceImpl implements TokenService {
     @Override
     @Transactional
     public LoginResponseDto generatesToken(LoginRequestDto requestDto) throws CredentialException {
-        try {
-            User user = userRepository.findByMail(requestDto.getMail())
-                    .orElseThrow(() -> new UserNotFoundException("No users with the attached resources were found"));
+        User user = userRepository.findByMail(requestDto.getMail())
+                .orElseThrow(() -> new UserNotFoundException("..."));
 
-            if (!user.isLoginCorrect(requestDto, passwordEncoder)) {
-                throw new CredentialException("Passwords don't match");
-            }
-            Instant now = Instant.now();
-            long expireIn = 1800L;
-
-            JwtClaimsSet claims = JwtClaimsSet.builder()
-                    .issuer("ondedoarbe")
-                    .subject(user.getUserId().toString())
-                    .issuedAt(now)
-                    .expiresAt(now.plusSeconds(expireIn))
-                    .build();
-
-            String jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-            log.info("Token encode claim configurations");
-
-            LoginResponseDto response = new LoginResponseDto();
-            response.setUserId(user.getUserId());
-            response.setAccessToken(jwtValue);
-            response.setExpireIn(expireIn);
-            log.info("Setting values for attributes");
-
-            return response;
-        } catch (RuntimeException e) {
-            log.error("It was not possible to generate a token", e);
-            throw e;
+        if (!Boolean.TRUE.equals(user.getActive())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Confirme seu e-mail...");
         }
+
+        if (!user.isLoginCorrect(requestDto, passwordEncoder)) {
+            throw new CredentialException("Passwords don't match");
+        }
+
+        return generateTokenForUser(user);
     }
+
+    public LoginResponseDto generateTokenForUser(User user) {
+        Instant now = Instant.now();
+        long expireIn = 1800L;
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("ondedoarbe")
+                .subject(user.getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expireIn))
+                .build();
+
+        String jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        LoginResponseDto response = new LoginResponseDto();
+        response.setUserId(user.getUserId());
+        response.setAccessToken(jwtValue);
+        response.setExpireIn(expireIn);
+
+        return response;
+    }
+
+    @Override
+    public String generatePasswordResetToken(User user) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("ondedoarbe")
+                .subject(user.getUserId().toString())
+                .claim("type", "PASSWORD_RESET")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(300))
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+
+    @Override
+    public String generateEmailVerificationToken(User user) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("ondedoarbe")
+                .subject(user.getUserId().toString())
+                .claim("type", "EMAIL_VERIFICATION")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(300))
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+
 }
