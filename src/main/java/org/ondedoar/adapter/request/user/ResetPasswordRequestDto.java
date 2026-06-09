46c5bde7bc23d510495f2ce8b8c1d298a6788ce3
luger-mano <lucas.germanysantos@gmail.com>
2026-06-09 13:58:29 -0300

    [feat] : ResetPasswordRequestDto criada para lidar com nova senha do usuário e mapeamento de endpoint em Security

---
+++ src/main/java/org/ondedoar/adapter/request/user/ResetPasswordRequestDto.java
@@ -0,0 +1,11 @@
+package org.ondedoar.adapter.request.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResetPasswordRequestDto {
+
+    private String password;
+}
